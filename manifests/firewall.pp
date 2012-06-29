102fcb0f057e336cc651e6b5fe59618c429d53e4
Justin Ellison <justin.ellison@buckle.com>
2012-06-29 11:00:29 -0500

    support for new firewall module update

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,7 +2,6 @@ class jenkins::firewall {
   if defined("firewall") and tagged("bke_firewall") {
     firewall { "500 allow jenkins inbound traffic":
       action => 'accept',
-      notify => Exec["firewall-save"],
       state  => "NEW",
       dport  => [8080],
       proto  => "tcp",
