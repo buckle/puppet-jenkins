c600b9ab85d24c8538770d03010aa8f99da6b6fb
Drew Blessing <Drew.Blessing@Buckle.com>
2012-11-01 15:38:35 -0500

    Fix config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,16 +1,16 @@
 class jenkins::config (
-  $home               = jenkins::params::home,
-  $java_cmd           = jenkins::params::java_cmd,
-  $user               = jenkins::params::user,
-  $java_options       = jenkins::params::java_options,
-  $port               = jenkins::params::port,
-  $ajp_port           = jenkins::params::ajp_port,
-  $debug_level        = jenkins::params::debug_level,
-  $enable_access_log  = jenkins::params::enable_access_log,
-  $handler_max        = jenkins::params::handler_max,
-  $handler_idle       = jenkins::params::handler_idle,
-  $args               = jenkins::params::args
-) {
+  $home               = $jenkins::params::home,
+  $java_cmd           = $jenkins::params::java_cmd,
+  $user               = $jenkins::params::user,
+  $java_options       = $jenkins::params::java_options,
+  $port               = $jenkins::params::port,
+  $ajp_port           = $jenkins::params::ajp_port,
+  $debug_level        = $jenkins::params::debug_level,
+  $enable_access_log  = $jenkins::params::enable_access_log,
+  $handler_max        = $jenkins::params::handler_max,
+  $handler_idle       = $jenkins::params::handler_idle,
+  $args               = $jenkins::params::args
+) inherits jenkins::params {
   file { "jenkins configuration file":
     owner         => root,
     group         => root,
