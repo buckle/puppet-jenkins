e009045134f87b8a7692156920c5ad0975eb7743
Drew Blessing <Drew.Blessing@Buckle.com>
2012-11-01 14:45:47 -0500

    Add config template

---
+++ manifests/config.pp
@@ -0,0 +1,25 @@
+class jenkins::config (
+  $home               = jenkins::params::home,
+  $java_cmd           = jenkins::params::java_cmd,
+  $user               = jenkins::params::user,
+  $java_options       = jenkins::params::java_options,
+  $port               = jenkins::params::port,
+  $ajp_port           = jenkins::params::ajp_port,
+  $debug_level        = jenkins::params::debug_level,
+  $enable_access_log  = jenkins::params::enable_access_log,
+  $handler_max        = jenkins::params::handler_max,
+  $handler_idle       = jenkins::params::handler_idle,
+  $args               = jenkins::params::args
+) {
+  file { "jenkins configuration file":
+    owner         => root,
+    group         => root,
+    mode          => 600,
+    path          => $osfamily ? {
+      'RedHat'    => "/etc/sysconfig/jenkins",
+      'Debian'    => "/etc/default/jenkins",
+      default     => "/etc/default/jenkins",
+    },
+    content       => template("jenkins/jenkins.erb")
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,35 @@
-class jenkins {
+class jenkins (
+  $home               = jenkins::params::home,
+  $java_cmd           = jenkins::params::java_cmd,
+  $user               = jenkins::params::user,
+  $java_options       = jenkins::params::java_options,
+  $port               = jenkins::params::port,
+  $ajp_port           = jenkins::params::ajp_port,
+  $debug_level        = jenkins::params::debug_level,
+  $enable_access_log  = jenkins::params::enable_access_log,
+  $handler_max        = jenkins::params::handler_max,
+  $handler_idle       = jenkins::params::handler_idle,
+  $args               = jenkins::params::args
+) {
   include jenkins::repo
   include jenkins::package
   include jenkins::service
+  
+  class { 'jenkins::config':
+    home               => $home,
+    java_cmd           => $java_cmd,
+    user               => $user,
+    java_options       => $java_options,
+    port               => $port,
+    ajp_port           => $ajp_port,
+    debug_level        => $debug_level,
+    enable_access_log  => $enable_access_log,
+    handler_max        => $handler_max,
+    handler_idle       => $handler_idle,
+    args               => $args
+  }
 
-  Class["jenkins::repo"] -> Class["jenkins::package"] -> Class["jenkins::service"]
+  Class["jenkins::repo"] -> Class["jenkins::package"] -> 
+  Class["jenkins::config"] -> Class["jenkins::service"]
 }
 # vim: ts=2 et sw=2 autoindent
